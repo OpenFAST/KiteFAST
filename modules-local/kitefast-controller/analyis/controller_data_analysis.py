1bbd2466023262cc491f825caed909629e41982e
justin <jmill2492@gmail.com>
2018-12-04 09:41:23 -0800

    added plotting module that accepts the output file from the controller and plots specified variables

---
+++ modules-local/kitefast-controller/analyis/controller_data_analysis.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+print "running contorller analysis"
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# user inputs
+plot_vars = ["sph_f.v","sph_f.alpha","flaps[0]", "rotors[7]"]
+file_name = "controller_save_data.csv"
+dT = 0.02 #sec
+row_count = 9 # total rows found in .csv
+header_count = 4
+
+# initialize variables
+plot_ind = []
+plot_vals = np.zeros([row_count-header_count,len(plot_vars)+1])
+time_val = 0
+temp_vals = np.zeros([len(plot_vars)+1])
+
+# TODO - have controller save the .csv to this directory
+with open(file_name) as file:
+#with open("modules-local/kitefast-controller/analyis/controller_save_data.csv") as file:
+	reader = csv.reader(file,delimiter=',')
+	line_count = 0
+	for row in reader:
+		# first header
+		if line_count == 0:
+			print(row)
+			line_count += 1
+		# file data created
+		elif line_count == 1:	
+			print(row)			
+			line_count += 1
+		# print controller version
+		elif line_count == 2:
+			print(row)				
+			line_count += 1
+		# sort through data header
+		elif line_count == 3:
+			i = 0
+			# grab header and make list
+			header = ', '.join(row)
+			header_list = [x.strip() for x in header.split(',')]
+			# find indicies of user inputted variables to plot	
+			while i < len(plot_vars):
+				if plot_vars[i] in header_list:
+					plot_ind.append(header_list.index(plot_vars[i]))
+				else:
+					msg = "Input " + plot_vars[i] + " is not a valid variable"
+					exit(msg)
+				i += 1
+			line_count += 1
+		else:
+			# assemble data of chosen variables
+			j = 0
+			while j < len(plot_vars)+1:
+				if j == 0:
+					temp_vals[j] = time_val
+					time_val += dT
+				else:
+					temp_vals[j] = row[plot_ind[j-1]]
+				j += 1
+			# assign to final structure  
+			plot_vals[line_count-header_count] = temp_vals 
+			line_count += 1
+# final data struct
+# print header
+print "Variables to be Plotted:"
+print(', '.join(plot_vars)) 
+print "Values:"
+print plot_vals
+print "Processed " ,line_count, " lines"
+
+#begin plotting
+k = 0
+while k < len(plot_vars):
+	plt.figure(k+1)
+	plt.title(plot_vars[k])
+	plt.plot(plot_vals[:,0], plot_vals[:,k+1], 'ro')
+	plt.axis([ np.min(plot_vals[:,0]), np.max(plot_vals[:,0]), np.min(plot_vals[:,k+1]), np.max(plot_vals[:,k+1]) ])
+	plt.show(block=True)
+	k += 1
--- modules-local/kitefast-controller/src/control/control_log.c
+++ modules-local/kitefast-controller/src/control/control_log.c
@@ -9,12 +9,11 @@ void ControlLogInit(char* controllerVerNumber){
     strftime(timeStr, sizeof(timeStr)-1, "%d/%m/%Y %H:%M \n", t);
 
 	FILE * fp;
-	fp = fopen("controller_save_data.txt", "w+");
+	fp = fopen("controller_save_data.csv", "w+");
 	fprintf(fp, "Controller Output Text File \nFile Created on: %s",timeStr);
 	fprintf(fp, "controller version: %s \n", controllerVerNumber);
-    fprintf(fp, "Header:  ");
-    fprintf(fp, "dcm[0,0], dcm[0,1], dcm[0,2], dcm[1,0], dcm[1,1], dcm[1,2], dcm[2,0], dcm[2,1], dcm[2,2], ");
-    fprintf(fp, "pqr_f.x, pqr_f.y, pqr_f.z, Xg.x, Xg.y, Xg.z, Vg.x, Vg.y, Vg.z, Vb.x, Vb.y, Vb.z, Ag.x, Ag.y, Ag.z, ");
+    fprintf(fp, "dcm0_0, dcm0_1, dcm0_2, dcm1_0, dcm1_1, dcm1_2, dcm2_0, dcm2_1, dcm2_2, ");
+    fprintf(fp, "acc_norm_f, pqr_f.x, pqr_f.y, pqr_f.z, Xg.x, Xg.y, Xg.z, Vg.x, Vg.y, Vg.z, Vb.x, Vb.y, Vb.z, Ag.x, Ag.y, Ag.z, ");
     fprintf(fp, "Ab_f.x, Ab_f.y, Ab_f.z, rho, sph_f.v, sph_f.alpha, sph_f.beta, tether_force_b.vector_f.x, tether_force_b.vector_f.y, tether_force_b.vector_f.z, ");
     fprintf(fp, "wind_g.vector.x, wind_g.vector.y, wind_g.vector.z, flaps[0], flaps[1], flaps[2], flaps[3], flaps[4], flaps[5], flaps[6], flaps[7], ");
     fprintf(fp, "rotors[0], rotors[1], rotors[2], rotors[3], rotors[4], rotors[5], rotors[6], rotors[7]");  
@@ -29,7 +28,7 @@ void ControlLogEntry(ControlLog* control_log){
     char assembledStr[1024];
     char tempStr[80];
     // dcm 
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.dcm_g2b.d[0][0],
         control_log->stateEstLog.dcm_g2b.d[1][0],
         control_log->stateEstLog.dcm_g2b.d[2][0],
@@ -38,71 +37,71 @@ void ControlLogEntry(ControlLog* control_log){
         control_log->stateEstLog.dcm_g2b.d[2][1],
         control_log->stateEstLog.dcm_g2b.d[0][2],
         control_log->stateEstLog.dcm_g2b.d[1][2],
-        control_log->stateEstLog.dcm_g2b.d[1][2]);
+        control_log->stateEstLog.dcm_g2b.d[2][2]);
     strcpy( assembledStr, tempStr);
     // pqr
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.pqr_f.x,
         control_log->stateEstLog.pqr_f.y,
         control_log->stateEstLog.pqr_f.z);
     strcat( assembledStr, tempStr);
     // acc_norm_c
-    sprintf(tempStr, "%0.4f\t", control_log->stateEstLog.acc_norm_f);
+    sprintf(tempStr, "%0.4f,", control_log->stateEstLog.acc_norm_f);
     strcat( assembledStr, tempStr);
     // Xg_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.Xg.x,        
         control_log->stateEstLog.Xg.y,
         control_log->stateEstLog.Xg.z);
     strcat( assembledStr, tempStr);
     // Vg_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.Vg.x,        
         control_log->stateEstLog.Vg.y,
         control_log->stateEstLog.Vg.z);
     strcat( assembledStr, tempStr);
     // Vb_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.Vb.x,        
         control_log->stateEstLog.Vb.y,
         control_log->stateEstLog.Vb.z);
     strcat( assembledStr, tempStr);
     // Ag_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.Ag.x,        
         control_log->stateEstLog.Ag.y,
         control_log->stateEstLog.Ag.z);
     strcat( assembledStr, tempStr);
     // Ab_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.Ab_f.x,        
         control_log->stateEstLog.Ab_f.y,
         control_log->stateEstLog.Ab_f.z);
     strcat( assembledStr, tempStr);
 	// rho
-    sprintf(tempStr, "%0.4f\t", control_log->stateEstLog.rho);
+    sprintf(tempStr, "%0.4f,", control_log->stateEstLog.rho);
     strcat( assembledStr, tempStr); 
 	//apparent_wind_c_v
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.apparent_wind.sph_f.v,        
         control_log->stateEstLog.apparent_wind.sph_f.alpha,
         control_log->stateEstLog.apparent_wind.sph_f.beta);
     strcat( assembledStr, tempStr);
 	//tether_force_c
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.tether_force_b.vector_f.x,        
         control_log->stateEstLog.tether_force_b.vector_f.y,
         control_log->stateEstLog.tether_force_b.vector_f.z);
     strcat( assembledStr, tempStr);
 	//wind_g
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,",
         control_log->stateEstLog.wind_g.vector.x,        
         control_log->stateEstLog.wind_g.vector.y,
         control_log->stateEstLog.wind_g.vector.z);
     strcat( assembledStr, tempStr);   
     // kFlaps[]
     printf("   Control Logging : saving flaps\n");
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,",
         control_log->controlOutputLog.flaps[kFlapA1],    
         control_log->controlOutputLog.flaps[kFlapA2],
         control_log->controlOutputLog.flaps[kFlapA4],
@@ -114,7 +113,7 @@ void ControlLogEntry(ControlLog* control_log){
     strcat( assembledStr, tempStr);
     printf("   Control Logging : saving rotors\n");
     // rotors 
-    sprintf(tempStr, "%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t%0.4f\t",
+    sprintf(tempStr, "%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,",
         control_log->controlOutputLog.rotors[kMotor1],    
         control_log->controlOutputLog.rotors[kMotor2],
         control_log->controlOutputLog.rotors[kMotor3],
@@ -126,18 +125,9 @@ void ControlLogEntry(ControlLog* control_log){
     strcat( assembledStr, tempStr); 
     
     FILE * fp;
-	fp = fopen("controller_save_data.txt", "a+");
-    
-    // double tempVal = ;
-    // sprintf(str, "%0.4f\t", control_log->stateEstLog.Ag.x);
-    // sprintf(str + strlen(str), "%0.4f\t", control_log->stateEstLog.Ag.y);
-    // sprintf(str + strlen(str), "%d\t", control_log->stateEstLog.Ag.z);
-    // char tempLogStr[];
-
-
+	fp = fopen("controller_save_data.csv", "a+");
 	fprintf(fp, "%s \n", assembledStr);
-	// fprintf(fp,"controller version: %s \n",controllerVerNumber);
 	fclose(fp);
 
-    printf("   Control Logging Initiated: File saved as - controller_save_data.txt \n");
+    printf("   Control Step Saved: File saved as - controller_save_data.csv \n");
 }
