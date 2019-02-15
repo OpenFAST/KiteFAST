5188c65d5b458a59b8b92eaaf91cf5f30b57ff0f
Greg Hayman <hayman.gregory@gmail.com>
2019-02-15 09:24:39 -0700

    Updated KFC c-based interfaces to fix bug in passing arrays
    changed varname(:) to varname(*)
    changed some of the scalar inputs to the c init interface to use the value attribute
    added some debug prints for testing
    added the aeroTorq input for the Step routine.

--- modules-local/kitefast-controller/src/KiteFastController.c
+++ modules-local/kitefast-controller/src/KiteFastController.c
@@ -5,10 +5,10 @@
 // KiteFastController.c
 // Intermediary wrapper between Kitefast and Shared Library
 
-void kfc_dll_init(double *dT, int *numFlaps, int *numPylons, 
-                  double I_rot[], double genTorq[], double rotorSpeed[],
-                  double rotorAccel[], double rotorBlPit[], 
-                  double ctrlSettings[],
+void kfc_dll_init(double dT, int numFlaps, int numPylons, 
+                  double* I_rot, double* genTorq, double* rotorSpeed,
+                  double* rotorAccel, double* rotorBlPit, 
+                  double* ctrlSettings,
                   int *errStat, char *errMsg) 
 {
     #ifdef DEBUG //DEBUG preproc found in kfc.h 
@@ -42,7 +42,7 @@ void kfc_dll_end(int *errStat, char *errMsg)
 void kfc_dll_step(double dcm_g2b_c[], double pqr_c[], double *acc_norm_c,
                   double Xg_c[], double Vg_c[], double Vb_c[], double Ag_c[],
                   double Ab_c[], double *rho_c, double apparent_wind_c[],
-                  double tether_force_c[], double wind_g_c[],
+                  double tether_force_c[], double wind_g_c[], double AeroTorque[],
                   double GenTorque[], double RotorSpeed[], 
                   double RotorAccel[], double RotorBladePitch[],
                   double CtrlSettings[],
@@ -51,7 +51,7 @@ void kfc_dll_step(double dcm_g2b_c[], double pqr_c[], double *acc_norm_c,
     #ifdef DEBUG //DEBUG preproc found in kfc.h
         printf("  debug marker - pre controller_step() \n");
     #endif
-    double AeroTorque[] = {};
+    //double AeroTorque[] = {};
     // Step call to Shared Library
     controller_step(dcm_g2b_c, pqr_c, acc_norm_c, 
                  Xg_c, Vg_c, Vb_c, Ag_c,
--- modules-local/kitefast-controller/src/KiteFastController.h
+++ modules-local/kitefast-controller/src/KiteFastController.h
@@ -15,10 +15,10 @@
 
 static int AbortErrLev = ErrID_Fatal;      // abort error level; compare with NWTC Library
 
-void kfc_dll_init(double *dT, int *numFlaps, int *numPylons, 
-                  double I_rot[], double genTorq[], double rotorSpeed[],
-                  double rotorAccel[], double rotorBlPit[], 
-                  double ctrlSettings[],
+void kfc_dll_init(double dT, int numFlaps, int numPylons, 
+                  double* I_rot, double* genTorq, double* rotorSpeed,
+                  double* rotorAccel, double* rotorBlPit, 
+                  double* ctrlSettings,
                   int *errStat, char *errMsg) ;               
                   
 void kfc_dll_end(int *errStat, char *errMsg);
@@ -26,7 +26,7 @@ void kfc_dll_end(int *errStat, char *errMsg);
 void kfc_dll_step(double dcm_g2b_c[], double pqr_c[], double *acc_norm_c,
                   double Xg_c[], double Vg_c[], double Vb_c[], double Ag_c[],
                   double Ab_c[], double *rho_c, double apparent_wind_c[],
-                  double tether_force_c[], double wind_g_c[],
+                  double tether_force_c[], double wind_g_c[], double AeroTorque[],
                   double GenTorque[], double RotorSpeed[], 
                   double RotorAccel[], double RotorBladePitch[],
                   double CtrlSettings[],
