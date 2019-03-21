6f76549fc54078e2d6e9f9c13b9923f7af6c9075
Rafael M Mudafort <rafmudaf@gmail.com>
2019-03-21 11:53:31 -0600

    Update the update script

--- glue-codes/kitefast/kitefast_update.sh
+++ glue-codes/kitefast/kitefast_update.sh
@@ -1,23 +1,21 @@
-# Build script for all KiteFAST related components on Debian Strech (9)
+# Update script for all KiteFAST related components on Debian Strech (9)
 
 # exit on error
 set -e
 
 ##### configuration
 
-source_code_parent_directory="/Users/rmudafor/Development/makani"
+source_code_parent_directory="/home/parallels/Desktop"
 mbdyn_directory=$source_code_parent_directory"/mbdyn-1.7.3"
 openfast_directory=$source_code_parent_directory"/makani_openfast"
 
-if [ ! -d $source_code_parent_directory ]; then
-  echo "source_code_parent_directory does not exist as given: "$source_code_parent_directory
-  exit 1
-fi
-cd $source_code_parent_directory
-
 #####
 
-### helpers
+##### helpers
+
+function print {
+  echo "*** "$@
+}
 
 require_clean_work_tree () {
     # Update the index
@@ -47,37 +45,41 @@ require_clean_work_tree () {
     fi
 }
 
-function print {
-  echo "*** "$@
-}
+#####
 
-# move into kitefast
+# check the directories
+if [ ! -d $source_code_parent_directory ]; then
+  print "source_code_parent_directory does not exist as given: "$source_code_parent_directory
+  exit 1
+fi
+
 if [ ! -d $openfast_directory ]; then
-   echo "openfast_directory does not exist as given: "$openfast_directory
+   print "openfast_directory does not exist as given: "$openfast_directory
    exit 1
 fi
-cd $openfast_directory
 
+if [ ! -d $openfast_directory/build ]; then
+  print "kitefast build directory does not exist as given: "$openfast_directory/build
+  exit 1
+fi
+
+if [ ! -d $mbdyn_directory ]; then
+  print "mbdyn_directory does not exist as given: "$mbdyn_directory
+  exit 1
+fi
+
+# rebuild kitefast
+cd $openfast_directory
 require_clean_work_tree "git checkout"
 git checkout dev
 git pull origin dev
-
-if [ ! -d build ]; then
-  mkdir build
-fi
 cd build
 cmake ..
 make -j 2 kitefastlib kitefastcontroller_controller
 
 # rebuild the mbdyn user module
-cd $source_code_parent_directory
-if [ ! -d $mbdyn_directory ]; then
-  echo "mbdyn_directory does not exist as given: "$mbdyn_directory
-  exit 1
-fi
 cd $mbdyn_directory
-sudo make                      # build mbdyn
-cd modules                     # move to the module directory
-sudo make                      # build the user defined element
-cd ..                          # move back to mbdyn
-sudo make install              # install everything
+cd modules                       # move to the module directory
+make clean && sudo make install  # build the user defined element
+cd ..                            # move back to mbdyn
+sudo make install                # install everything
