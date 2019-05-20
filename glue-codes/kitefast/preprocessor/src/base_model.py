4cae2195cc222a9133f516fd42ae0fc1ae529eac
Rafael M Mudafort <rafmudaf@gmail.com>
2019-05-20 16:52:57 -0600

    Preprocessor: add the deep_put method

--- glue-codes/kitefast/preprocessor/src/base_model.py
+++ glue-codes/kitefast/preprocessor/src/base_model.py
@@ -29,11 +29,42 @@ class BaseModel():
         return Vec3(list[0], list[1], list[2])
 
     def _deep_get(self, _dict, keys, default=None):
+        """
+        Recursive function for finding a nested dict.
+
+        _dict: the dictionary to search over
+        keys: list of keys defining the nested path
+        default: optional value to return when the given path is not found
+
+        returns the nested dictionary
+        """
         for key in keys:
             if isinstance(_dict, dict):
                 _dict = _dict.get(key, default)
             else:
                 return default
+        return _dict
+
+    def _deep_put(self, _dict, keys, value):
+        """
+        A function for putting a given value at a key path.
+
+        _dict: the input dictionary to modify
+        keys: list of keys defining the nested path
+        value: the value to add at the nested path
+
+        returns the modified input dictionary
+
+        NOTE: this takes advantage of the face that Python stores values by
+        reference. Since `traverse_dict` is referencing the same memory as the
+        input `_dict`, modifying it also modifies `_dict`.
+        """
+        traverse_dict = _dict
+        for i, key in enumerate(keys[:-1]):
+            traverse_dict = traverse_dict.get(key)
+            if i == len(keys) - 2:
+                traverse_dict[keys[-1]] = value
+                break
         return _dict
 
     def _verify_component_list(self, model_dict):
