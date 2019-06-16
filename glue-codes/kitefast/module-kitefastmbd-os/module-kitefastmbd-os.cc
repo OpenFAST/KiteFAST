2d3939f995990cb777889f8c84b35ac745c7b234
Rafael M Mudafort <rafmudaf@gmail.com>
2019-06-15 20:37:15 -0600

    Preprocessor: add the platform loads to workvec

--- glue-codes/kitefast/module-kitefastmbd-os/module-kitefastmbd-os.cc
+++ glue-codes/kitefast/module-kitefastmbd-os/module-kitefastmbd-os.cc
@@ -1060,6 +1060,17 @@ SubVectorHandler &ModuleKiteFASTOS::AssRes(SubVectorHandler &WorkVec, doublereal
     WorkVec.Add(6 * node_count_no_rotors + 6 * i + 4, moment);
   }
 
+  integer first_index = platform_node.pNode->iGetFirstMomentumIndex();
+  for (int j = 1; j <= 6; j++)
+  {
+    WorkVec.PutRowIndex(6 * (node_count_no_rotors + n_rotor_points) + j, first_index + j);
+  }
+  silent_cout("platform_loads: " << platform_loads[0] << "\t" << platform_loads[1] << "\t" << platform_loads[2] << std::endl);
+  Vec3 force = Vec3(platform_loads[0], platform_loads[1], platform_loads[2]);
+  Vec3 moment = Vec3(platform_loads[3], platform_loads[4], platform_loads[5]);
+  WorkVec.Add(6 * (node_count_no_rotors + n_rotor_points) + 1, force);
+  WorkVec.Add(6 * (node_count_no_rotors + n_rotor_points) + 4, moment);
+
   delete[] node_loads;
   delete[] rotor_loads;
   delete[] platform_loads;
